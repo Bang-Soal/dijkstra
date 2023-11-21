8e6a6263c0cb77f85577ad971a9d444a789e86b1
Imo Madjid <mhmmadjid@gmail.com>
2023-11-22 01:40:25 +0700

    feat: added scrollable selector to sidebar

--- src/app/latihan-soal/components/Sidebar.tsx
+++ src/app/latihan-soal/components/Sidebar.tsx
@@ -6,6 +6,7 @@ import bgMeshVertical from "@/public/bg-mesh-vertical.webp";
 // components
 import Iconify from "@components/Iconify";
 import Filters from "./Filters";
+import SoalSelector from "./SoalSelector";
 
 // libs
 import * as Accordion from "@radix-ui/react-accordion";
@@ -32,7 +33,7 @@ const accordians = [
 
 export default function Sidebar() {
   return (
-    <nav className="flex w-80 flex-col gap-6 border-r border-surface-300 bg-surface-200">
+    <nav className="flex h-screen w-80 flex-col gap-6 border-r border-surface-300 bg-surface-200">
       <div className="flex h-16 items-center justify-center gap-2 text-xl font-700 text-content-200">
         <p className="">Bang Soal</p>
         <p className="text-surface-400">|</p>
@@ -59,7 +60,7 @@ export default function Sidebar() {
             <Accordion.Item
               key={accordian.title}
               value={`item-${index + 1}`}
-              className="group relative flex flex-col overflow-hidden rounded-xl bg-cover data-[state=open]:grow data-[state=closed]:animate-slide-up-item data-[state=open]:animate-slide-down-item"
+              className="group relative flex flex-col rounded-xl bg-cover data-[state=closed]:h-11 data-[state=open]:grow data-[state=closed]:animate-slide-up-item data-[state=open]:animate-slide-down-item data-[state=closed]:overflow-hidden data-[state=open]:overflow-visible"
             >
               <Accordion.Trigger className="group z-10 flex w-full items-center gap-2 rounded-xl px-3 py-2 text-xl font-[650] text-content-300 outline-none transition-colors data-[state=open]:cursor-default data-[state=open]:text-surface-100 data-[state=closed]:focus-within:text-content-100 data-[state=closed]:hover:text-content-100">
                 <Iconify icon={accordian.icon} className="" />
@@ -69,15 +70,21 @@ export default function Sidebar() {
                   className="transition-transform duration-500 group-data-[state=open]:rotate-180"
                 />
               </Accordion.Trigger>
-              <Accordion.Content className="z-10 grow px-1 pb-1 data-[state=closed]:animate-slide-up-content data-[state=open]:animate-slide-down-content">
+              <Accordion.Content
+                forceMount
+                className="z-10 flex grow flex-col px-1 pb-1 data-[state=closed]:h-0 data-[state=open]:h-[--radix-accordian-content-height] data-[state=closed]:animate-slide-up-content data-[state=open]:animate-slide-down-content"
+              >
                 <Filters
+                  category={accordian.title as "PU" | "PK" | "PPU" | "PBM"}
+                />
+                <SoalSelector
                   category={accordian.title as "PU" | "PK" | "PPU" | "PBM"}
                 />
               </Accordion.Content>
               <Image
                 src={bgMeshVertical}
                 alt={""}
-                className="pointer-events-none absolute inset-0 object-cover transition-[opacity] duration-500 group-data-[state=closed]:opacity-0 group-data-[state=open]:opacity-100"
+                className="pointer-events-none absolute inset-0 h-full rounded-xl object-cover transition-[opacity] duration-500 group-data-[state=closed]:opacity-0 group-data-[state=open]:opacity-100"
                 aria-hidden="true"
               />
             </Accordion.Item>
