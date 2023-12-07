a2e7b7cb63d7d60231fc434535b697b011e7e129
Imo Madjid <mhmmadjid@gmail.com>
2023-12-07 15:17:58 +0700

    feat: added iluustrations and class join utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { clsx, type ClassValue } from "clsx";
+import { twJoin, twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function join(...inputs: ClassValue[]) {
+  return twJoin(clsx(inputs));
 }
