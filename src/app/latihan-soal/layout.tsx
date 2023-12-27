f1e71d540f3cad3ddca2a6bf98e7b66751d404c5
umarez <umar.izzuddin@ui.ac.id>
2023-12-27 11:19:05 +0700

    feat: add mathpix latex cdn

--- src/app/latihan-soal/layout.tsx
+++ src/app/latihan-soal/layout.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { MathpixMarkdownModel as MM } from "mathpix-markdown-it";
+import { useEffect } from "react";
 import withAuth from "../components/withAuth";
 import SoalAside from "./components/SoalAside";
 import { LatihanSoalProvider } from "./context";
@@ -8,6 +10,17 @@ function LatihanSoalLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  useEffect(() => {
+    return () => {
+      const elStyle = document.getElementById("Mathpix-styles");
+      if (!elStyle) {
+        const style = document.createElement("style");
+        style.setAttribute("id", "Mathpix-styles");
+        style.innerHTML = MM.getMathpixFontsStyle() + MM.getMathpixStyle(true);
+        document.head.appendChild(style);
+      }
+    };
+  }, []);
   return (
     <LatihanSoalProvider>
       <div className="flex min-h-screen w-full flex-row">
