862518f8605e629c319f1a5a7624438acc2d62e2
Catherine Angel <catheriner40540@gmail.com>
2024-01-08 20:59:43 +0700

    chore: remove unused query

--- src/app/dashboard/latihan-soal/LatihanSoalSettings.tsx
+++ src/app/dashboard/latihan-soal/LatihanSoalSettings.tsx
@@ -1,11 +1,8 @@
-import { useGetHeadersDashboardQuery } from "@/redux/api/dashboardApi";
 import { Catatanmu } from "./Catatanmu";
 import { LatsolOverview } from "./LatsolOverview";
 import { TopicStatistics } from "./TopicStatistics";
 
 export const LatihanSoalSettings = () => {
-  const { data } = useGetHeadersDashboardQuery();
-
   return (
     <div className="flex flex-col gap-5 pb-4">
       <LatsolOverview />
